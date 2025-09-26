c9f35117f5456ffa2781674cac02ce74dde61b42
livlevi <91070792+livlevi@users.noreply.github.com>
2025-09-25 22:00:15 -0400

    change ami ID

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test_instance" {
-    ami = "ami-0fd3ac4abb734302a"
+    ami = "ami-08982f1c5bf93d976"
     instance_type = "t2.nano"
     tags = {
         Name = "test_instance02"
