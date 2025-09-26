ad155cfb533b0b5007f475ff35fe66c5681309dc
livlevi <91070792+livlevi@users.noreply.github.com>
2025-09-25 21:54:26 -0400

    test pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "test_instance" {
     ami = "ami-830c94e3"
     instance_type = "t2.nano"
     tags = {
-        Name = "test_instance"
+        Name = "test_instance02"
     }
 }
