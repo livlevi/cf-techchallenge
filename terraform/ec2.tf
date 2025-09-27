3e52c8f762cdceb48949b9642c61be463f1faae8
livlevi <91070792+livlevi@users.noreply.github.com>
2025-09-27 19:10:09 -0400

    add kms config

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,6 +10,8 @@ module "challenge_ec2" {
     vpc_id = module.challenge_vpc.vpc_id
     subnet_ids = module.challenge_vpc.private_subnets["-subnet02-public-us-east-1b"]
 
+    ec2_key_pair = module.ec2_test.kms_key_id
+
     root_volume_size = 50
 
     ingress_rules = {
