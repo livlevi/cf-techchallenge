854229ccbc578449ca89d9c8e63afc05729fc432
livlevi <91070792+livlevi@users.noreply.github.com>
2025-09-29 00:07:59 -0400

    added public to private route table

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -107,10 +107,8 @@ resource "aws_security_group" "public_security_group" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http_public" {
 
-    for_each = module.challenge_vpc.public_subnets_cidr_blocks
-
     security_group_id = aws_security_group.public_security_group.id
-    cidr_ipv4 = each.value
+    cidr_ipv4 = ["0.0.0.0/0"]
     from_port = 80
     ip_protocol = "tcp"
     to_port = 80
